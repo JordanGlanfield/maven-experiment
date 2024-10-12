b18cb79fca3eadee0bb0e7b72a94d21e96624d61
jordan <j.glanfield@hotmail.co.uk>
2024-10-12 18:28:49 +0100

    fix mvn test not running tests

--- src/main/java/org/jordan/Main.java
+++ src/main/java/org/jordan/Main.java
@@ -1,7 +1,14 @@
 package org.jordan;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello again Java");
+
+        var sum = new SummationProcessor(Arrays.asList(1, 2, 3, 4)).process();
+
+        System.out.printf("The sum is %d%n", sum);
+
     }
 }
